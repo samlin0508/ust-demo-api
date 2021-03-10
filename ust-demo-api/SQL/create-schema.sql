067935f5a22e62beada7abbd3fcb0ec0a754fabd
Sam Lin <sam.lin@bionime.com>
2021-03-10 14:13:15 +0800

    added sql scripts.

---
+++ ust-demo-api/SQL/create-schema.sql
@@ -0,0 +1,10 @@
+﻿CREATE DATABASE `ust-demo` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+CREATE TABLE `reminder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `summary` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8_unicode_ci,
+  `createDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updateDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
